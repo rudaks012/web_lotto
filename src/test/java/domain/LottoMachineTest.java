bb2ab4749c9ce3a5e4b49bd5305ba6fcdb1d4e49
gyeongman <rudaks012@naver.com>
2022-09-11 15:39:58 +0900

    Test : 로또 머신 테스트 코드 작성

---
+++ src/test/java/domain/LottoMachineTest.java
@@ -0,0 +1,39 @@
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import weblotto.domain.LottoMachine;
+import weblotto.domain.LottoQuantity;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LottoMachineTest {
+
+    @ParameterizedTest(name = "로또 금액 대비 개수 확인 :[{index}] : [{arguments}]")
+    @DisplayName("로또 금액 대비 개수 확인")
+    @CsvSource(value = {
+            "11000,11",
+            "1000,1"
+    })
+    void inputLottoAmount(int amount, int ticketCount) {
+        // given
+        LottoQuantity lottoQuantity = new LottoQuantity(amount);
+        // when
+
+        // then
+        assertThat(lottoQuantity.lottoQuantity()).isEqualTo(ticketCount);
+    }
+
+    @Test
+    @DisplayName("로또 구매금액 천원 미만 유효성 확인")
+    void zeroAmountExceptionTest() {
+        // given
+        int amount = 0;
+        // when // then
+        assertThatThrownBy(() -> new LottoMachine(amount))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
