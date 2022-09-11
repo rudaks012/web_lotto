613b00daf7f65da09bcb5b6006f1782b96dcab90
gyeongman <rudaks012@naver.com>
2022-09-11 17:29:23 +0900

    Test : 로또 Yield 관련 테스트 코드 작성

--- src/test/java/domain/LottoYieldTest.java
+++ src/test/java/domain/LottoYieldTest.java
@@ -1,2 +1,62 @@
-package domain;public class LottoYieldTest {
+package domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import weblotto.domain.LottoYield;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class LottoYieldTest {
+
+    @Test
+    @DisplayName("수익 통계 일치 확인")
+    void lotto_yield_true_test() {
+        // given
+        double profit = 1;
+        LottoYield lottoYield = new LottoYield(profit);
+        // when
+        boolean actual = lottoYield.checkYield(profit);
+        // then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    @DisplayName("수익 통계 불일치 확인")
+    void lotto_yield_false_test() {
+        // given
+        double profit = 1;
+        double checkProfit = 2;
+        LottoYield lottoYield = new LottoYield(profit);
+        // when
+        boolean actual = lottoYield.checkYield(checkProfit);
+        // then
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    @DisplayName("이익 문자열 확인")
+    void string_profit_test() {
+        // given
+        double profit = 1;
+        LottoYield lottoYield = new LottoYield(profit);
+        String expected = "이익";
+        // when
+        String actual = lottoYield.stringYield();
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("손해 문자열 확인")
+    void string_loss_test() {
+        // given
+        double profit = 0.99;
+        LottoYield lottoYield = new LottoYield(profit);
+        String expected = "손해";
+        // when
+        String actual = lottoYield.stringYield();
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
 }
